2db7871cf7b9c79142cdabe2947f0c09adc4ed67
merch2 <merch2@yandex.ru>
2016-03-04 16:53:51 +0600

    fix111

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,6 @@ namespace :deploy do
   desc 'Restart app'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute :touch, release_path.join('tmp/restart.txt')
       invoke 'unicorn:restart'
     end
   end
