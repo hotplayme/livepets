e6654d93854944a1b52ae9d2fd02170db413bbca
merch2 <merch2@yandex.ru>
2016-03-30 17:35:23 +0600

    111

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_to, '/home/rails'
 set :deploy_user, 'rails'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', 'public/robots.txt', 'public/sitemap.xml.gz')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', 'public/robots.txt', 'public/sitemap.xml.gz', 'public/favicon.ico')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/images', 'public/photos', 'public/avatars')
